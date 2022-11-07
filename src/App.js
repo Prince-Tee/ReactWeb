ca91ae3efe6e7944b843ea6ae16395ec970775aa
codedlibra <mrcoded98@gmail.com>
2022-11-06 23:39:38 -0800

    added styling to contact and profile page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import Contact from "./components/pages/Contact";
 import Footer from "./components/pages/Footer";
 import {Profile} from "./components/pages/Profile"
