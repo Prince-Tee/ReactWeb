b50e119ef2ed64054c4a36c19297344a8e47e354
codedlibra <mrcoded98@gmail.com>
2022-11-06 22:14:19 -0800

    refactor: added bootstrap for styling and restructured the app.js, contact page

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
+import {Routes, Route, Link} from 'react-router-dom';
 import taiwo from './components/taiwo.jpg'
 import './App.css';
 import Contact from './pages/Contact';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-  
-} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-        <div className="App">
-    
-    <Routes>
-    <Route path="/contact" element={<Contact />} />
-    <Route path='/navvv' element={<navvv />}/>
-    </Routes> 
-    
-     <div className='container'>
-     <header>
-    <img id='profile__img' alt='profilepic' src={taiwo}  />
-    </header>
+     <div className="App">
+        <Routes>
+        <Route path="/contact" element={<Contact />} />
+        <Route path='/' element={<App/>}/>
+        </Routes> 
 
-    <main>
-    <h1 id='twitter'> ADEBIYI1605</h1>
-    <h1 id='slack'> Taiwo Adebiyi </h1>
-    <button><a  href='https://training.zuri.team/' id='btn__zuri'>TRANING</a></button>
-    <button><a href=' http://books.zuri.team' id='books'>This is where you find books about design and coding</a></button>
-    <button><a href='https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>' id='book__python'>ZURI PYTHON BOOKS</a></button>
-    <button><a href='https://background.zuri.team' id='pitch'>Background checks on coders</a></button>
-    <button><a href='https://books.zuri.team/design-rules' id='book__design'>ZURI free design books</a></button>
-    <button> <a   href='/contact' id='contact'> Contact Me</a> </button>
-    
-     </main>
-    
-     
-     
-     
+        <div className='container'>
+        <header>
+        <img id='profile__img' alt='profilepic' src={taiwo}  />
+        </header>
+
+        <main>
+        <h1 id='twitter'> ADEBIYI1605</h1>
+        <h1 id='slack' style={{display:none}}> Taiwo Adebiyi </h1>
+        <div>
+          <a  href='https://training.zuri.team/' 
+          id='btn__zuri'>TRANING</a>
+        </div>
+        <div>
+          <a href=' http://books.zuri.team' 
+          id='books'>This is where you find books about design and coding</a>
+        </div>
+        <div>
+          <a href='https://books.zuri.team/python-for-beginners?ref_id=ADEBIYI1605' 
+          id='book__python'>ZURI PYTHON BOOKS</a>
+        </div>
+        <div>
+          <a href='https://background.zuri.team' 
+          id='pitch'>Background checks on coders</a></div>
+        <div><a href='https://books.zuri.team/design-rules' id='book__design'>ZURI free design books</a></div>
+        <div>
+          <Link to='/contact' 
+          id='contact'> Contact Me</Link>
+          </div>
+        
+        </main>
+        </div>
+      
     </div>
-  
-    </div>
-    
-    </Router>
+        
   );
 }
 
